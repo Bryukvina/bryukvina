28b8657162abdbabee33e42788549755c2139b9d
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-04-12 19:17:08 +0300

    Update acd_1.py

--- acd_1.py
+++ acd_1.py
@@ -1,3 +1,4 @@
+
 import RPi.GPIO as GPIO
 import time
 
@@ -5,43 +6,34 @@ dac = [26, 19, 13, 6, 5, 11, 9, 10]
 bits = len(dac)
 levels = 2 ** bits
 maxVoltage = 3.3
-comp = 
+comp = 4
 troyka = 17
-
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(troyka, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(comp, GPIO.IN)
 
-def decimal2binary(decimal):
-    return [int(bit) for bit in bin(decimal)[2:].zfill(bits)]
-
 def num2dac(value):
-    signal = decimal2binary(value)
-    GRIO.output(dac, signal)
+    signal = [int(bit) for bit in bin(value)[2:].zfill(bits)]
+    GPIO.output(dac, signal)
     return signal
 
 def adc():
-    fv = 256    
     for value in range(256):
-        signal = num2dac(value)
-        compValue = GPIO.input(comp)
-        if comp == 1:
-            fv = min(fv, value)
-        time.sleep(0.01)
-    return fv
+        num2dac(value)
+        time.sleep(0.001)
+        if GPIO.input(comp) == 0:
+            return value
+
+    return 255
 
 try:
     while True:
         value = adc()
         voltage = value / levels * maxVoltage
         print("ADC value: {:^3}, input Voltage = {:.2f}".format(value, voltage))
-except KeyboardInterrupt:
-    print("The program was stopped by the keyboard")
-else:
-    pass
+
 finally:
-    p.stop()
-    GPIO.cleanup(channal)
+    GPIO.cleanup()
     print("GPIO cleanup complited")
